415e21788716a070b0da1b1dd65375a94cb1b485
Ariiq354 <realgoldcraft@gmail.com>
2023-08-07 21:48:55 +0700

    change image

--- app/(informasi)/inovasi/page.tsx
+++ app/(informasi)/inovasi/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Inovasi() {
   return (
     <>
@@ -6,59 +8,13 @@ export default function Inovasi() {
           Inovasi Puskesmas
         </h1>
       </div>
-      <div className="container my-20 justify-center flex flex-wrap lg:my-24">
-        <div className="columns-sm">
-          <p className="tracking-widest">Inovasi Puskesmas</p>
-          <h1 className="font-bold text-xl">
-            Kebuli Seceting (Kelas ibu peduli gizi serentak cegah stunting)
-          </h1>
-          <div>
-            <p>
-              Stunting ialah kondisi gagal tumbuh pada anak balita akibat
-              kekurangan gizi kronis terutama dalam 1000 Hari pertama kehidupan.
-            </p>
-            <p className="break-inside-avoid">
-              Faktor : <br />
-              &nbsp;&nbsp;&nbsp;• Kurang Gizi dalam Waktu Lama <br />
-              &nbsp;&nbsp;&nbsp;• Pola Asuh KurangEfektif <br />
-              &nbsp;&nbsp;&nbsp;• Pola Makan <br />
-              &nbsp;&nbsp;&nbsp;• Tidak Melakukan Perawatan Pasca Melahirkan{" "}
-              <br />
-              &nbsp;&nbsp;&nbsp;• Gangguan Mental dan Hipertensi Pada Ibu <br />
-              &nbsp;&nbsp;&nbsp;• Faktor Sanitasi <br />
-            </p>
-            <p className="break-inside-avoid">
-              Dampak : <br />
-              &nbsp;&nbsp;&nbsp;• Perkembangan otak tergangu <br />
-              &nbsp;&nbsp;&nbsp;• Kemampuan kognitif terhambat <br />
-              &nbsp;&nbsp;&nbsp;• Mudah terserang penyakit <br />
-              &nbsp;&nbsp;&nbsp;• Beresiko tinggi terserang penyakit
-              degenerative <br />
-            </p>
-            <p className="break-inside-avoid">
-              Pencegahan : <br />
-              &nbsp;&nbsp;&nbsp;• Memenuhi kebutuhan asupan ibu dan bayi <br />
-              &nbsp;&nbsp;&nbsp;• Pemberian ASI Esklusif dan MP-ASI <br />
-              &nbsp;&nbsp;&nbsp;• Pemantauan tumbuh kembang anak <br />
-              &nbsp;&nbsp;&nbsp;• Akses air bersih dan fasilitas sanitasi <br />
-            </p>
-            <p className="break-inside-avoid">
-              Gejala : <br />
-              &nbsp;&nbsp;&nbsp;• Wajah tampak lebih muda dari anak seusianya{" "}
-              <br />
-              &nbsp;&nbsp;&nbsp;• Pertumbuhan tubuh dan gigi yang terlambat{" "}
-              <br />
-              &nbsp;&nbsp;&nbsp;• Memiliki kemampuan fokus dan memori belajar
-              yang buruk <br />
-              &nbsp;&nbsp;&nbsp;• Pubertas yang lambat <br />
-              &nbsp;• Saat menginjak usia 8-10 tahun, anak cenderung lebih
-              pendiam dan tidak banyak melakukan kontak mata dengan orang
-              sekitarnya <br />
-              &nbsp;&nbsp;&nbsp;• Berat badan lebih ringan untuk anak seusianya
-            </p>
-          </div>
-        </div>
-        <div></div>
+      <div className="container flex justify-center my-6">
+        <Image
+          src="/inovasi/inovasi.jpg"
+          alt=""
+          width={1000}
+          height={1000}
+        ></Image>
       </div>
     </>
   );
